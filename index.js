39778ebd8a0d3c3cead4ace89e628b1779f0b346
unknown <malinovskiy925@gmail.com>
2023-05-25 23:52:01 +0300

    hw 5 done

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-
-let posElemSum = arr.reduce(function(accum, elem) {
-	if (elem > 0) {
-		return accum + elem;
-	} else {
-		return accum;
-	}
-}, 0);
-console.log("Сумма позитивных элементов", posElemSum);
-let posElemNumber = arr.filter(function(elem) {
-	if (elem > 0) {
-		return true;
-	} else {
-		return false;
-	}
-});
-console.log("Количество позитивных элементов", posElemNumber.length);
-console.log("Минимальный элемент", Math.min(...arr));
-console.log("Порядковый номер минимального элемента", arr.indexOf(Math.min(...arr)) + 1);
-console.log("Максимальный элемент", Math.max(...arr));
-console.log("Подярковый номер максимального элемента", arr.indexOf(Math.max(...arr)) + 1);
-let negElemNumber = arr.filter(function(elem) {
-	if (elem < 0) {
-		return true;
-	} else {
-		return false;
-	}
-});
-console.log("Количество негативных элементов", negElemNumber.length);
-let oddPosElemNumber = arr.filter(elem => elem % 2 == 1 && elem > 0);
-console.log("Количество нечетных позитивных элементов", oddPosElemNumber.length);
-let evenPosElemNumber = arr.filter(elem => elem % 2 == 0 && elem > 0);
-console.log("Количество четных позитивных элементов", evenPosElemNumber.length);
-let evenPosElemSum = arr.reduce(function(accum, elem) {
-	if (elem % 2 == 0 && elem > 0) {
-		return accum + elem;
-	} else {
-		return accum;
-	}
-}, 0);
-console.log("Сумма четных позитивных элементов", evenPosElemSum);
-let oddPosElemSum = arr.reduce(function(accum, elem) {
-	if (elem % 2 == 1 && elem > 0) {
-		return accum + elem;
-	} else {
-		return accum;
-	}
-}, 0);
-console.log("Сумма нечетных позитивных элементов", oddPosElemSum);
-let posElem = arr.filter(elem => (elem > 0));
-let posElemMult = posElem.reduce((accum, elem) => accum * elem);
-console.log("Произведение позитивных элементов", posElemMult);
-let maxElem = Math.max(...arr);
-let finalResult = arr.map(x => x == maxElem ? x : 0);
-console.log("Наибольший элемент массива оставить, остальные = 0", finalResult);
+let users = [
+    {
+    "index": 0,
+    "isActive": true,
+    "balance": "$2,226.60",
+    "name": "Eugenia Sawyer",
+    "gender": "female",
+    "phone": "+1 (840) 583-3207",
+    "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+    "index": 1,
+    "isActive": true,
+    "balance": "$2,613.77",
+    "name": "Pauline Gallegos",
+    "gender": "female",
+    "phone": "+1 (985) 593-3328",
+    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+    "index": 2,
+    "isActive": false,
+    "balance": "$3,976.41",
+    "name": "Middleton Chaney",
+    "gender": "male",
+    "phone": "+1 (995) 591-2478",
+    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+    "index": 3,
+    "isActive": true,
+    "balance": "$1,934.58",
+    "name": "Burns Poole",
+    "gender": "male",
+    "phone": "+1 (885) 559-3422",
+    "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+    "index": 4,
+    "isActive": true,
+    "balance": "$3,261.65",
+    "name": "Mcfadden Horne",
+    "gender": "male",
+    "phone": "+1 (942) 565-3988",
+    "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+    "index": 5,
+    "isActive": false,
+    "balance": "$1,790.56",
+    "name": "Suzette Lewis",
+    "gender": "female",
+    "phone": "+1 (837) 586-3283",
+    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+    }
+    ];
+    let richUsers = users.filter((user) => user.balance > "$2,000.00");
+    let richUsersPhones = richUsers.map((user) => user.phone);
+    console.log("Phone numbers of users, who has more than 2000$:", richUsersPhones);
+    let totalBalance = users.map((user) => Number(user.balance.replace("$", "").replace(",", "")));
+    let sumBalance = totalBalance.reduce((accum, user) => {
+        return accum + user;
+    }, 0);
+    console.log("Sum of all balances of users:", sumBalance);
