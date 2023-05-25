5cde12f1e04f86eafb2905f10a0af778a7cd0ad0
unknown <malinovskiy925@gmail.com>
2023-05-25 22:39:47 +0300

    hw 3 done

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-/*
-let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-if(numOrStr === null) {
-console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-console.log(' number is Ba_NaN')
-} else {
-console.log('OK!')
-}
-*/
-
-let numOrStr = prompt('input number or string');
-console.log(numOrStr);
-
-switch (true) {
-    case (numOrStr === null): console.log('ви скасували')
-    break;
-    case (numOrStr.trim() === ""): console.log('Empty String')
-    break;
-    case isNaN(+numOrStr): console.log('number is Ba_NaN')
-    break;
-    default: console.log('OK!')
-}
+const userInput = prompt("Enter some numbers, symbols or words divided with space");
+const array = userInput.split(" ");
+console.log(array);
+array.sort();
+console.log(array);
+array.splice(1, 3);
+console.log(array);
