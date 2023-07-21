64e8eef14a22ce043a10f35a760326686be55c6b
unknown <malinovskiy925@gmail.com>
2023-07-21 17:15:27 +0300

    hw14 done

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
-const counter = document.querySelectorAll(".counter");
-function click(event) {
-    if (event.target.tagName === "IMG") {
-        const countElement = event.target.nextElementSibling;
-        let count = countElement.innerHTML;
-        count++;
-        countElement.innerHTML = count;
+const store = [
+    {
+        position: 1,
+        name: "Keyboard",
+        priceInUAH: 2000,
+        color: "Black",
+        type: "Mechanics"
+    },
+    {
+        position: 2,
+        name: "Mouse",
+        priceInUAH: 600,
+        color: "Yellow",
+        DPI: 12000
+    },
+    {
+        position: 3,
+        name: "Monitor",
+        priceInUAH: 5000,
+        color: "Grey",
+        quality: "full HD"
+    },
+    {
+        position: 4,
+        name: "PC",
+        priceInUAH: 15000,
+        color: "White",
+        OS: "Windows"
+    },
+    {
+        position: 5,
+        name: "Headphones",
+        priceInUAH: 400,
+        color: "Yellow",
+        Microphone: "Yes"
+    }
+];
+function addOrder(productId) {
+    const savedOrders = JSON.parse(localStorage.getItem("userOrders")) || [];
+    const date = new Date().toLocaleString();
+    const product = store.find(item => item.position === productId);
+    if (product) {
+        const order = {
+            date: date,
+            productId: productId
+        };
+        savedOrders.push(order);
+        localStorage.setItem("userOrders", JSON.stringify(savedOrders));
     }
 }
-const images = document.querySelector(".container").addEventListener("click", click);
-const styleSheet = document.styleSheets[0];
-styleSheet.insertRule(".container {display: flex; justify-content: center; gap: 10px}");
-styleSheet.insertRule(".counter {display: flex; justify-content: center}")
+function showOrders() {
+    const savedOrders = JSON.parse(localStorage.getItem("userOrders")) || [];
+    const itemsToBuy = document.querySelector(".itemsToBuy");
+    itemsToBuy.style.display = "none";
+    const ordersContainer = document.createElement("div");
+    ordersContainer.classList.add("ordersContainer");
+    savedOrders.forEach(order => {
+        const orderElement = document.createElement("div");
+        orderElement.classList.add("order");
+        const productId = order.productId;
+        const product = store.find(item => item.position === productId);
+        if (product) {
+            const productName = product.name;
+            const productPrice = product.priceInUAH;
+
+            orderElement.innerHTML = `<p>Date: ${order.date}</p><p>Item: ${productName}</p><p>Price: ${productPrice} UAH</p>`;
+            const deleteButton = document.createElement("button");
+            deleteButton.innerText = "Remove";
+            deleteButton.addEventListener("click", () => deleteOrder(order));
+            orderElement.appendChild(deleteButton);
+
+            ordersContainer.appendChild(orderElement);
+        }
+    });
+    document.body.appendChild(ordersContainer);
+}
+function deleteOrder(order) {
+    const savedOrders = JSON.parse(localStorage.getItem("userOrders")) || [];
+    const index = savedOrders.findIndex(savedOrder => savedOrder.productId === order.productId);
+    if (index !== -1) {
+        savedOrders.splice(index, 1);
+        localStorage.setItem("userOrders", JSON.stringify(savedOrders));
+    }
+    const ordersContainer = document.querySelector(".ordersContainer");
+    ordersContainer.remove();
+    showOrders();
+}
+const ordersButton = document.querySelector(".ordersButton");
+ordersButton.addEventListener("click", showOrders);
+const buyButtons = document.querySelectorAll(".buyButton");
+buyButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const productId = parseInt(event.target.dataset.id);
+        addOrder(productId);
+    });
+});
