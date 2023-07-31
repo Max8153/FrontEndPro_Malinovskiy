472598c9660f22f79ea9addf3a6e7f85fc4bfe90
unknown <malinovskiy925@gmail.com>
2023-07-31 22:59:00 +0300

    hw17 done

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const createHouseBtn = document.querySelector(".createHouseBtn").addEventListener("click", showHouseForm);
-const createHouseForm = document.querySelector(".createHouseForm");
-function showHouseForm(event) {
-    event.preventDefault();
-    createHouseForm.style.display = "block";
-}
-const numberOfFlats = document.querySelector("#flats");
-const peapleInEachFlat = document.querySelector("#peaple");
-const create = document.querySelector("#create").addEventListener("click", showResult);
-let flats;
-let peaple;
-let house1;
-const totalPeaple = () => flats*peaple;
-function showResult(event) {
-    event.preventDefault();
-    flats = parseInt(numberOfFlats.value);
-    peaple = parseInt(peapleInEachFlat.value);
-    house1 = new House(flats, peaple);
-    house1.showHouse();
-    console.log(`And in this house live ${totalPeaple()} peaple at all.`)
-}
-class House {
-    constructor(flats, peaple) {
-        this.flats = flats;
-        this.peaple = peaple;
+class Burger {
+    constructor(size, stuffing) {
+        this.size = size;
+        this.stuffing = stuffing;
+        this.topping = [];
     }
-    showHouse() {
-        console.log(`Ok, here is(are) ${flats} flat(s) it the house with ${peaple} peaple(person) in each flat.`);
+    static sizeSmall = {price: 50, calories: 20};
+    static sizeBig = {price: 100, calories: 40};
+    static withCheese = {price: 10, calories: 20};
+    static withSalad = {price: 20, calories: 5};
+    static withPotato = {price: 15, calories: 10};
+    static toppingSpice = {price: 15, calories: 0};
+    static toppingMayo = {price: 20, calories: 5};
+    addTopping(topping) {
+        this.topping.push(topping);
+    }
+    calcPrice() {
+        const totalPrice = this.size.price + this.stuffing.price + this.topping.reduce((accum, topping) => accum + topping.price, 0);
+        return totalPrice;
+    }
+    calcCalories() {
+        const totalCalories = this.size.calories + this.stuffing.calories + this.topping.reduce((accum, topping) => accum + topping.calories, 0);
+        return totalCalories;
     }
 }
+const burger1 = new Burger(Burger.sizeBig, Burger.withCheese);
+burger1.addTopping(Burger.toppingSpice);
+console.log("Price: " + burger1.calcPrice());
+console.log("Calories: " + burger1.calcCalories());
